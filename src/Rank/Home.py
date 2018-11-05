b32540bbebc6b090493637e5f391f420543100b3
qiyyyue <qiyyyue@gmail.com>
2018-11-05 13:44:55 +0800

    Sec version

--- src/Rank/Home.py
+++ src/Rank/Home.py
@@ -113,6 +113,55 @@ def get_visitor_leaderboard():
     except:
         return json.dumps({"code": "False", 'msg': 'query fail'}, ensure_ascii=False)
 
+@home.route("/GetTeacherLeaderBoard", methods=['POST', 'GET'])
+def get_teacher_leaderboard():
+    continent = request.form.get('continent', None)
+    country = request.form.get('country', None)
+    city = request.form.get('city', None)
+
+    period = request.form.get('period', None)
+
+    try:
+        user_list = query_users_by_loc(continent, country, city)
+        rank_list = []
+        for user_id in user_list:
+            user_info = query_userinfo_by_id(user_id)
+            user_name = user_info[0]
+            if period == "Daily":
+                point = performance_query_daily_by_id(user_id)
+                old_point = performance_query_pre_daily_by_id(user_id)
+            elif period == "Weekly":
+                point = performance_query_weekly_by_id(user_id)
+                old_point = performance_query_pre_weekly_by_id(user_id)
+            elif period == "Monthly":
+                point = performance_query_monthly_by_id(user_id)
+                old_point = performance_query_pre_monthly_by_id(user_id)
+            else:
+                point = performance_query_all_by_id(user_id)
+                old_point = performance_query_pre_daily_by_id(user_id)
+            rank_list.append({'username': user_name, 'continent': user_info[1], 'country': user_info[2], 'city': user_info[3], 'point': point, 'old_point': old_point})
+        rank_list.sort(key=lambda user_info: user_info['old_point'], reverse = True)
+        for old_rank in range(len(rank_list)):
+            rank_list[old_rank]['old_rank'] = old_rank + 1
+
+        rank_list.sort(key=lambda user_info: user_info['point'], reverse=True)
+        for i in range(len(rank_list)):
+            rank_list[i]['rank'] = i + 1
+            tmp = rank_list[i]['rank'] - rank_list[i]['old_rank']
+            if tmp < 0:
+                rank_list[i]['status'] = '+' + str(abs(tmp))
+            elif tmp > 0:
+                rank_list[i]['status'] = '-' + str(abs(tmp))
+            else:
+                rank_list[i]['status'] = '--'
+
+        # print(rank_list)
+
+
+        return json.dumps({"code": "True", 'msg': '', "leader_board_list": rank_list}, ensure_ascii=False)
+    except:
+        return json.dumps({"code": "False", 'msg': 'query fail'}, ensure_ascii=False)
+
 ##获取排行榜信息
 ##continent, country, city, period, username.
 ##返回{code, msg, leader_board_list, self_info}
